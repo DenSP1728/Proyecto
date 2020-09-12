4c600ce8180d92c033a1141902f14d60015e6d42
DenSP1728 <70044558+DenSP1728@users.noreply.github.com>
2020-09-11 21:01:01 -0500

    Update avances.py

--- avances.py
+++ avances.py
@@ -1,5 +1,20 @@
-# Condiciones para avanzar en juego, donde el número mayor es el jugador que empieza 
-   
+#AVANCE 3 ESTRUCTURA DE DESICIONES.
+#Eleccion de color para el jugador, escogiendo un color del 1 al 3, si no le toca verde 
+def elegir_color (color):
+    return color
+color = int (input("Escoge color:   "))
+
+if color == 1:
+    print ("Color de jugador es:", "amarillo")
+elif color == 2:
+    print ("Color de jugador es:", "verde")
+elif color == 3:
+    print ("Color de jugador es:", "azul")
+else:
+    print ("Color de jugador es:", "rojo")
+      
+      
+# Condiciones para avanzar en juego, donde el número mayor es el jugador que empieza    
 def casillas (jugador_1, jugador_2, jugador_3):
     return jugador_1 or jugador_2 or jugador_3
 
@@ -20,7 +35,8 @@ elif jugador_3 > jugador_1 and jugador_3 > jugador_2:
 
 #El jugador deberá sacar ficha si la suma de ambos dados es un 5, por ejemplo 2+3.
 #También es posible sacar 5 y 5 y sacar dos fichas en el mismo turno.
-        
+
+#---OPERADORES CON DESICION---        
 def sacar_ficha (dados_1, dado_2):
     return dados_1, dado_2
 
