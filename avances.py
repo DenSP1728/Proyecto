ac3d8c9dfa8379c01471564a01b17b7a412024bd
DenSP1728 <70044558+DenSP1728@users.noreply.github.com>
2020-10-09 23:25:13 -0500

    Update avances.py

--- avances.py
+++ avances.py
@@ -1,4 +1,7 @@
+
 import random
+import os    #Biblioteca para limpiar la ventana 
+import sys   #Biblioteca para cerrar el codigo automaticamente 
 
 #Definimos el tablero
 tablero= [["*****","*****","*****","*****","_____","_____","_____","*****","*****","*****","*****"],
@@ -13,22 +16,31 @@ tablero= [["*****","*****","*****","*****","_____","_____","_____","*****","****
           ["*****","*****","*****","*****","_____","_____","_____","*****","*****","*****","*****"],
           ["*****","*****","*****","*****","_____","_____","_____","*****","*****","*****","*****"]]
 
-def menu():
-    print('Hola, este es el juego de Parchis')
+def limpiar():     #Funcion para limpiar la ventana de codigo
+    if os.name=='posix':
+        os.system('clear')
+    elif os.name=='ce' or os.name=='nt' or os.name=='dos':
+        os.system('cls')
+
+def salir():    #Funcion para salir del juego automaticamente 
+    limpiar()
+    print('Se acabo el juego')
+    input('Presiona Intro para salir')
+    sys.exit()
+
+def instrucciones():
+    limpiar()
+    print(' -Instruciones de juego- ')
     print()
-    print('Presiona 1 para jugar')
+    print('1. Para comenzar el juego cada jugador registra su nombre')
+    print('2. Para sacar ficha de casa es necesario tirar 5')
+    print('3. Gana el jugador que primero llegue a la meta')
     print()
-    print('Presiona 2 para ver instrucciones')
-    print()
-    print('Presiona 3 para salir')
-    a = input('Respuesta: ')
-    if a == '1':
-         jugar()   #funcion que conecte con menu, sin definir
-    elif a == '2':
-         instrucciones()   #sin definir
-    elif a == '3':
-         salir()   #sin definir 
-def dado():
+    input('Presiona Intro para regresar a menu')
+    menu()
+    salir()    
+
+def dado(fic):  #fichas
     dado = random.randint (1,6)
     
     if dado == 1:
@@ -87,16 +99,106 @@ def dado():
         print ("Sacaste", dado)
         
     else:
-        dado1 = [[" __"," __"," __"],
+        dado1 =  [[" __"," __"," __"],
                  ["/o ","   ","  o/"],
                  ["/o ","   ","  o/"],
                  ["/o "," __"," _o/"]]
-        for i in range (3):
-            for j in range (4):
+        for i in range (4):
+            for j in range (3):
                 print (dado1[i][j], end = "")
             print ()
         print ("Sacaste", dado)
-print(dado())
+    limpiar()
+    
+    if fic==1:
+        sacar_fichaA(dado)   #Asiganda a jugador 1
+    elif fic==2:
+        sacar_fichaB(dado,2)   #Para jugador 2
+    else:                   # 2 si es contra maquina
+        sacar_fichaB(dado, 1)  
+
+def game(x):
+    limpiar()
+    j1=input('Ingresa nombre del Jugador 1: ')
+    print()
+    if x==2:
+        j2=input('Ingresa nombre del Jugador 2: ')
+    else:
+        pass
+    for i in range(11):
+        for j in range(11):
+            print(tablero[i][j], end='')
+        print()
+    q = 0
+    while q==0 and x==2:
+        
+        if tablero[0][0]=='1****' and tablero[0][1]=='2****':
+            print('Gano', j1, '¡Congratulations!')
+            input('Presiona Intro para salir')
+            salir()
+        elif tablero[10][10]=='1****' and tablero[10][9]=='2****':
+            print('Gano', j2, '¡Congratulations!')
+            input('Presiona Intro para salir')
+            salir()
+        else:
+            print('Juega', j1)
+            input('Presiona Intro para continuar')
+            dado(1)
+            print('Juega', j2)
+            input('Presiona Intro para continuar')
+            dado(2)
+    while q==0 and x==1:
+        
+        if tablero[0][0]=='1****' and tablero[0][1]=='2****':
+            print('Gano', j1, '¡Congratulations!')
+            input('Presiona Intro para salir')
+            salir()
+        elif tablero[10][10]=='1****' and tablero[10][9]=='2****':
+            print('Gana la maquina, suerte para la proxima')
+            input('Presiona Intro para salir')
+            salir()
+        else:
+            print('Juega', j1)
+            input('Presiona Intro para continuar')
+            dado(1)
+            print('Sigue jugando la maquina')
+            input('Presiona Intro para continuar')
+            dado(0)
+            #global tablero
+
+def jugadores():
+    limpiar()
+    print('Selecciona los jugadores')
+    print()
+    print('Presiona 1 para jugar contra la maquina')
+    print('Presiona 2 para dos jugaores')
+    print()
+    print('Presiona cualquier tecla para volver al menu')
+    a = input('Respuesta: ')
+    if a=='1':
+        game(1)
+    elif a=='2':
+        game(2)
+    else:
+        limpiar()
+        menu()
+        
+def menu():  
+    print('Hola, este es el juego de Parchis')
+    print()
+    print('Presiona 1 para jugar')
+    print()
+    print('Presiona 2 para ver instrucciones')
+    print()
+    print('Presiona 3 para salir')
+    a= input('Respuesta: ')
+    if a== '1':
+       jugadores()
+    elif a== '2':
+       print(instrucciones())
+    elif a== '3':
+       print(salir())
+menu()
 
 #_______MODIFICAR_______
 #AVANCE 3 ESTRUCTURA DE DESICIONES.
@@ -114,24 +216,7 @@ elif color == 3:
 else:
     print ("Color de jugador es:", "rojo")
       
-      
-# CONDICIONES: para avanzar en juego, donde el número mayor es el jugador que empieza    
-def casillas (jugador_1, jugador_2, jugador_3):
-    return jugador_1 or jugador_2 or jugador_3
-
-jugador_1 = int(input())
-jugador_2 = int(input())
-jugador_3 = int(input())
-    
-if jugador_1 > jugador_2 and jugador_1 > jugador_3:
-        print ("comienza partida","jugador_1")
-        
-elif jugador_2 > jugador_1 and jugador_2 > jugador_3:
-        print ("comienza partida","jugador_2")
-        
-elif jugador_3 > jugador_1 and jugador_3 > jugador_2:
-        print ("comienza partida","jugador_3")
-               
+     
 
 
 #El jugador deberá sacar ficha si la suma de ambos dados es un 5, por ejemplo 2+3.
@@ -152,16 +237,4 @@ while True:
         break
     
 
-  #---Para hacer pruebas con datos de juego Parchis con Quiz----
-def dado (dados):
-    return dados
-
-dado=int(input())
-
-if dado in range (1, 13):
-    print("avanza")
-
-else:
-    print ("número de dado inválido")
-    
-    #NOTA: cambie el codigo para que fuera en un solo if
+ 
